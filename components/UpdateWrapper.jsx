b0ec44c2c7d6a9201213688003cd9a44544c7eff
Kushwahadheeraj <kushwahadheeraj245@gmail.com>
2025-06-24 10:21:10 +0530

    UpdateWrapper Update commit

--- components/UpdateWrapper.jsx
+++ components/UpdateWrapper.jsx
@@ -1,17 +1,13 @@
-"use client";
-import { usePathname } from "next/navigation";
-import Update from "@/app/Update/Update";
-import Navbar from "./Navbar";
+'use client';
+import { usePathname } from 'next/navigation';
+import Update from '@/app/Update/Update';
+import Navbar from './Navbar';
 
 export default function UpdateWrapper() {
   const pathname = usePathname();
   const hideUpdate =
-    pathname.startsWith("/login/seller") ||
-    pathname.startsWith("/dashboard");
+    pathname.startsWith('/login/seller') || pathname.startsWith('/dashboard');
 
   if (hideUpdate) return null;
-  return(
-<Update />,
-<Navbar />
-  )
+  return (<Update />), (<Navbar />);
 }

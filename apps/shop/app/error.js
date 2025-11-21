9df7de7fedd21d5a4136e6fe9d39b6cd2cad667c
Kushwahadheeraj <kushwahadheeraj245@gmail.com>
2025-11-21 11:25:54 +0530

    update

--- apps/shop/app/error.js
+++ apps/shop/app/error.js
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
 export default function Error({ error, reset }) {
   return (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>Something went wrong!</h1>
+    <div>
+      <h2>Something went wrong!</h2>
       <button onClick={() => reset()}>Try again</button>
     </div>
   );
--- apps/shop/pages/404.js
+++
@@ -1,9 +0,0 @@
-export default function Custom404() {
-  return (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>404</h1>
-      <p>Page Not Found</p>
-    </div>
-  );
-}
-

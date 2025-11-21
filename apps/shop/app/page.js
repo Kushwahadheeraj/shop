38e44c80d8b19846e471914dcb856d32c1b3eb6a
Kushwahadheeraj <kushwahadheeraj245@gmail.com>
2025-11-21 10:58:04 +0530

    update

--- apps/shop/app/page.js
+++ apps/shop/app/page.js
@@ -1,5 +1,5 @@
-import HomeSection from './Home/HomeSection';
-import Update from './Update/Update';
+import HomeSection from './Home/_components/HomeSection';
+import Update from './Update/_components/Update';
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
